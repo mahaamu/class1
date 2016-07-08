3de41fd226a432f591ffaddb9597cdb23dfc2df6
mahaamu <mahalakshmi3901@gmail.com>
2016-07-08 11:40:00 +0530

    Update and rename README.md to README.c
    finish

---
+++ README.c
@@ -0,0 +1,15 @@
+# class1
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+int a;
+printf("enter the no");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("number is even");
+else
+printf("number is odd");
+}
+}
